62247adade968e3bb11818bda4690e48a63c7fa8
Franz Taborlupa <franztaborlupa@gmail.com>
2020-10-29 10:26:26 +0800

    Deployment + Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+    <footer>
+      <div class="footer-primary">
+        <img src="../assets/hackfest_logo.svg" alt="hackfest-logo" />
+        <p>Hackfest 2020 is the Developer Student Club Loyola's (DSC) flagship organised by students for students.</p>
+        <img src="#">
+      </div>
+      <div class="footer-secondary">
+        <div class="footer-menu">
+          <h6>About</h6>
+          <span>Vision</span>
+          <span>Mission</span>
+        </div>
+        <div class="footer-menu">
+          <h6>Help</h6>
+          <span>Registration</span>
+          <span>Sponsorships</span>
+        </div>
+      </div>
+    </footer>
+
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style>
+  footer {
+    display: flex;
+    flex-direction: column;
+    height: 332px;
+    width: 100%;
+  }
+</style>
--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -61,6 +61,31 @@
 
 </template>
 
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBm3i5DJQ_fNBy_TIgrf3TtMkTmvzOR-Zc",
+    authDomain: "hackfest-2020.firebaseapp.com",
+    databaseURL: "https://hackfest-2020.firebaseio.com",
+    projectId: "hackfest-2020",
+    storageBucket: "hackfest-2020.appspot.com",
+    messagingSenderId: "658920405863",
+    appId: "1:658920405863:web:02b69259783a2740018dc2",
+    measurementId: "G-BTJFBSN1XQ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+
 <script>
 export default {
   name: 'Landing'
