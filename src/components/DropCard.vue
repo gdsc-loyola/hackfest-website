7fb8e0ca45cbb92150689f3dd9a9f949fa7bcedb
tanzellgo <58267330+tanzellgo@users.noreply.github.com>
2020-11-05 10:08:23 +0800

    schedule repsonsive and ready (#10)

---
+++ src/components/DropCard.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="drop-card" @click="toggle">
+        <div class="drop-header">
+            <slot name="header"></slot>
+            <img src="../assets/arrow.svg" 
+                alt="arrow" 
+                v-bind:style="[this.isOpen ? {'transform': 'rotate(90deg)'} : {'transform':' rotate(0deg)'}]" 
+            />
+        </div>
+        <div v-if="this.isOpen" class="drop-details" >
+            <hr>
+            <slot name="details"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DropCard',
+  data: () => {
+    return {
+      isOpen: false
+    }
+  },
+
+  methods: {
+    toggle: function () {
+      this.isOpen = !this.isOpen;  
+    }
+  }
+}
+</script>
+
+<style>
+
+.drop-card {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  width: 100%;
+  padding: 30px 20px;
+  border: 1px solid #4285F4;
+  box-sizing: border-box;
+  border-radius: 20px;
+  margin: 20px 0;
+}
+
+.drop-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+
+.drop-header img {
+  transition: all 0.5s
+}
+
+.drop-details hr {
+  margin: 15px 0;
+}
+
+</style>
