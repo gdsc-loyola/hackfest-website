939701ffc3fa32d28189a7a059fc8543fd3af16e
Franz Taborlupa <franztaborlupa@gmail.com>
2020-11-07 02:48:59 +0800

    Add partner assets

--- src/pages/Partners.vue
+++ src/pages/Partners.vue
@@ -4,43 +4,17 @@
     <div class="partners-wrapper">
       <div class="gold-partners">
         <h2>Gold</h2>
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
+        <img src="../assets/partners/gold/Accenture logo_Masterbrand.png">
+        <img src="../assets/partners/gold/SGVlogo_bbww_2015.png">
+        <img src="../assets/partners/gold/WIM Logo (PNG).png">
       </div>
       <div class="silver-partners">
         <h2>Silver</h2>
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
-        <img src="../assets/Ellipse 46.png">
+        <img src="../assets/partners/silver/Ambidextr_logo_positive.png">
+        <img src="../assets/partners/silver/MVN PHOTOSTUDIO_logo222.png">
+        <img src="../assets/partners/silver/NP_Logo_Horizontal_Colored.png">
+        <img src="../assets/partners/silver/SparkUp_logo_navy.png">
+        <img src="../assets/partners/silver/UXPH Logo Full Colour CMYK.png">
       </div>
     </div>
   </main>
